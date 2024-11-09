7291fcc9a90a6fe98dea98f3b5daa707cac78dac
2fill <ichaeu@naver.com>
2024-11-09 12:42:04 +0900

    Feat: lottie 애니메이션 프로토타입 구현

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,37 +1,51 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
+import {Player} from '@lottiefiles/react-lottie-player';
 import './Chat.css';
+import animationData from './animation1.json';
 
 function Chat(){
     const[messages, setMessages] = useState([]);
     const[input, setInput] = useState('');
+    const[showAnimation, setShowAnimation] = useState(false);
 
     const handleSend = () => {
         if(input.trim() === '') return;
 
         setMessages([...messages, input]);
         setInput('');
+        
+        setShowAnimation(true);
+        setTimeout(() => setShowAnimation(false), 2490);
     };
 
     return(
-        <div className = "chat-body">
-            <div className = "chat-container">
-                <h2>Chat</h2>
-                <div className = "message-box">
-                    {messages.map((msg, index) => (
-                        <div key = {index} className = "message">
-                            {msg}
-                        </div>
-                    ))}
-                </div>
-                <div className = "input-box">
-                    <input
-                        type = "text"
-                        value = {input}
-                        onChange = {(e) => setInput(e.target.value)}
-                        placeholder = "메시지를 입력하세요."
+        <div className = "chat-container">
+            <h2>Chat</h2>
+            {showAnimation && (
+                <div className = "animation">
+                    <Player 
+                        autoplay
+                        loop = {false}
+                        src = {animationData}
+                        style = {{height: '100px', width: '100px'}}
                     />
-                    <button onClick = {handleSend}>📩</button>
                 </div>
+            )}
+            <div className = "message-box">
+                {messages.map((msg, index) => (
+                    <div key = {index} className = "message">
+                        {msg}
+                    </div>
+                ))}
+            </div>
+            <div className = "input-box">
+                <input
+                    type = "text"
+                    value = {input}
+                    onChange = {(e) => setInput(e.target.value)}
+                    placeholder = "메시지를 입력하세요."
+                />
+                <button onClick = {handleSend} className = "send-button">📩</button>
             </div>
         </div>
     );
