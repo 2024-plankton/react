d7447596602a2a2f0dffd0e0d7a84bb6bc37c49f
Marsboy02 <marsboy0619@gmail.com>
2024-11-10 00:48:17 +0900

    feat: 엔터키를 누르면 메시지가 보내지는 기능 추가

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,72 +2,72 @@ import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 // import { Player } from "@lottiefiles/react-lottie-player";
 import "./Chat.css";
-import botProfile from './img/logo_smile_2.png';
-import btnPointer from './img/btn_pointer.png';
+import botProfile from "./img/logo_smile_2.png";
+import btnPointer from "./img/btn_pointer.png";
 
 function Chat() {
-    const [messages, setMessages] = useState([]);
-    const [input, setInput] = useState("");
-    const [setShowAnimation] = useState(false);
-    const [showModal, setShowModal] = useState(false);
-    const [clickedMessage, setClickedMessage] = useState(null);
-    const messageBoxRef = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+  const [setShowAnimation] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [clickedMessage, setClickedMessage] = useState(null);
+  const messageBoxRef = useRef(null);
 
-    useEffect(() => {
-        if(messageBoxRef.current){
-            messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
-        }
-    }, [messages]);
+  useEffect(() => {
+    if (messageBoxRef.current) {
+      messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+    }
+  }, [messages]);
 
-    const handleSend = async () => {
-        if (input.trim() === "") return;
+  const handleSend = async () => {
+    if (input.trim() === "") return;
 
-        // 사용자 입력 메시지를 추가
-        setMessages([...messages, {type: 'user', text: input}]);
+    // 사용자 입력 메시지를 추가
+    setMessages([...messages, { type: "user", text: input }]);
 
-        // API 요청 보내기
-        try {
-            const chatServerUrl = process.env.REACT_APP_CHAT_SERVER_URL;
-            const response = await axios.post(`${chatServerUrl}/chat`, {
-            name: "John",
-            query: input,
-            });
+    // API 요청 보내기
+    try {
+      const chatServerUrl = process.env.REACT_APP_CHAT_SERVER_URL;
+      const response = await axios.post(`${chatServerUrl}/chat`, {
+        name: "John",
+        query: input,
+      });
 
-            // 서버 응답을 messages 상태에 추가
-            setMessages((prevMessages) => [...prevMessages, {type: 'bot', text: response.data.responses[0].text},
-            ]); // 서버에서 응답 메시지를 response.data.reply로 가정합니다.
-        }
-        catch (error) {
-            console.error("메시지 전송 오류:", error);
-            setMessages((prevMessages) => [
-                ...prevMessages,
-                {type: 'bot', text: "Error: 메시지 전송 실패"},
-            ]);
-        }
+      // 서버 응답을 messages 상태에 추가
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "bot", text: response.data.responses[0].text },
+      ]); // 서버에서 응답 메시지를 response.data.reply로 가정합니다.
+    } catch (error) {
+      console.error("메시지 전송 오류:", error);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "bot", text: "Error: 메시지 전송 실패" },
+      ]);
+    }
 
-        // 입력 필드 비우기
-        setInput("");
+    // 입력 필드 비우기
+    setInput("");
 
-        // 애니메이션 시작
-        setShowAnimation(true);
-        setTimeout(() => setShowAnimation(false), 2490);
-    };
+    // 애니메이션 시작
+    setShowAnimation(true);
+    setTimeout(() => setShowAnimation(false), 2490);
+  };
 
-    const handleShowModal = (msgType) => {
-        if(msgType === 'bot'){
-            setClickedMessage(msgType);
-            setShowModal(true);
-        }
-        else {
-            setClickedMessage(null);
-            setShowModal(false);
-        }
-    };
+  const handleShowModal = (msgType) => {
+    if (msgType === "bot") {
+      setClickedMessage(msgType);
+      setShowModal(true);
+    } else {
+      setClickedMessage(null);
+      setShowModal(false);
+    }
+  };
 
-    return (
-        <div className="chat-wrapper">
-            <div className="chat-container">
-                {/* {showAnimation && (
+  return (
+    <div className="chat-wrapper">
+      <div className="chat-container">
+        {/* {showAnimation && (
                     <div className="animation">
                         <Player
                             autoplay
@@ -77,55 +77,63 @@ function Chat() {
                         />
                     </div>
                 )} */}
-                <div className="message-box" ref = {messageBoxRef}>
-                    {messages.map((msg, index) => (
-                        <div
-                            key={index}
-                            className={`message ${msg.type}`}
-                            onClick={() => handleShowModal(msg.type)}
-                        >
-                        {msg.type === 'user' ? (
-                            <div className = "user-message">
-                                <div className = "message-text">{msg.text}</div>  
-                            </div>
-                        ) : (
-                            <div className = "message-bot">
-                                <img src = {botProfile} alt = "Bot Profile" className = "bot-profile" />
-                                <div className = "bot-message">
-                                    {msg.text}
-                                </div>
-                            </div>
-                            )}
-                        </div>
-                    ))}
+        <div className="message-box" ref={messageBoxRef}>
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`message ${msg.type}`}
+              onClick={() => handleShowModal(msg.type)}
+            >
+              {msg.type === "user" ? (
+                <div className="user-message">
+                  <div className="message-text">{msg.text}</div>
                 </div>
-                <div className="input-box">
-                    <input
-                        type="text"
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        placeholder="메시지를 입력하세요."
-                    />
-                    <button onClick={handleSend} className="send-button">
-                        <img
-                            src = {btnPointer}
-                            alt = "Button Pointer"
-                            style = {{
-                                maxWidth: '20px',
-                                maxHeight: '20px',
-                                objectFit: 'contain'
-                            }} />
-                    </button>
+              ) : (
+                <div className="message-bot">
+                  <img
+                    src={botProfile}
+                    alt="Bot Profile"
+                    className="bot-profile"
+                  />
+                  <div className="bot-message">{msg.text}</div>
                 </div>
+              )}
             </div>
-            {showModal && clickedMessage === 'bot' && (
-                <div className="modal">
-                    <p>Modal Content</p>
-                <button onClick={() => setShowModal(false)}>Close</button>
-                </div>
-            )}
+          ))}
         </div>
-    );
+        <div className="input-box">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="메시지를 입력하세요."
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSend();
+              }
+            }}
+          />
+          <button onClick={handleSend} className="send-button">
+            <img
+              src={btnPointer}
+              alt="Button Pointer"
+              style={{
+                maxWidth: "20px",
+                maxHeight: "20px",
+                objectFit: "contain",
+              }}
+            />
+          </button>
+        </div>
+      </div>
+      {showModal && clickedMessage === "bot" && (
+        <div className="modal">
+          <p>Modal Content</p>
+          <button onClick={() => setShowModal(false)}>Close</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Chat;
